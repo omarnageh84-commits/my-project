179799935a3a8fdb70a22101720837b19ca48e49
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-15 02:00:24 +0300

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'omar-v1';
+const FILES = ['./','./index.html','./home.html','./daily.html','./attendance.html','./tasks.html','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
