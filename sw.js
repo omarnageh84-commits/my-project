fb08e38f185e7c77a9baa66d9c8787349060ac95
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-15 02:25:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,34 @@
-const CACHE = 'omar-v1';
-const FILES = ['./','./index.html','./home.html','./daily.html','./attendance.html','./tasks.html','./manifest.json'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+const CACHE_NAME = 'my-project-v2';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './home.html',
+  './attendance.html',
+  './daily.html',
+  './tasks.html',
+  './manifest.json',
+  './sheets.js',
+  './gmail.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
