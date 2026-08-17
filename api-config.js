3a02be7548beef2c7c7aa6119754a113b889fef3
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-17 16:43:49 +0300

    Create api-config.js

---
+++ api-config.js
@@ -0,0 +1,2 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwZK86VIWCxTAm0id556i3akziyE1oT8Qk75Zs2WRzR3zaZn-WqKOfGPi8_zXcPcwEImA/exec";
+const SECRET_KEY = "omar_secret_2024"; // ده نفس الـ KEY اللي في الـ Code.gs عندك في Apps Script
