49e27ff336fccddbb31ea0d0c9ef40bf3f866f1d
omarnageh84-commits <omarnageh84@gmail.com>
2026-08-17 12:50:30 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,30 +1,13 @@
-// firebase.js - المخ بتاع التلات صفحات
+// firebase.js - نسخة جاهزة للـ GitHub Pages
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, doc, setDoc, getDocs, deleteDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, doc, setDoc, deleteDoc, onSnapshot, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 👇 حط الـ Config الحقيقي هنا
 const firebaseConfig = {
-  apiKey: "هتحط_الـAPI_KEY_هنا",
+  apiKey: "هتحط_الـAPI_KEY_هنا", // <-- غير ده
   authDomain: "omar-project.firebaseapp.com",
   projectId: "omar-project",
   storageBucket: "omar-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  messagingSenderId: "123456789", // <-- وده
+  appId: "1:123456789:web:abcdef" // <-- وده
 };
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// دوال عامة عشان نستخدمها في التلات صفحات
-export async function saveData(colName, id, data){
-  await setDoc(doc(db, colName, id), data);
-}
-export async function deleteData(colName, id){
-  await deleteDoc(doc(db, colName, id));
-}
-export function listenData(colName, callback){
-  onSnapshot(collection(db, colName), (snap)=>{
-    let arr = [];
-    snap.forEach(d=> arr.push({id: d.id, ...d.data()}));
-    callback(arr);
-  });
-}
